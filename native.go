cf7a9a0f39887a2ea5792992a45a36938629cacc
Admin <2762713521@qq.com>
2024-10-17 09:25:08 +0800

    NativeTypeBind

--- native.go
+++ native.go
@@ -14,25 +14,25 @@ type (
 )
 
 // NativeTypeBind  for struct filed
-var NativeTypeBind = map[reflect.Kind]BaseKind{
-	reflect.Int8:                       IntegerKind,
-	reflect.Int16:                      IntegerKind,
-	reflect.Int32:                      IntegerKind,
-	reflect.Int64:                      IntegerKind,
-	reflect.Uint8:                      IntegerKind,
-	reflect.Uint16:                     IntegerKind,
-	reflect.Uint32:                     IntegerKind,
-	reflect.Uint64:                     IntegerKind,
-	reflect.String:                     StringKind,
-	reflect.Slice:                      BinaryKind, //[]byte blob BINARY list
-	reflect.Struct:                     StructKind,
-	reflect.Array:                      ListKind,
-	reflect.Map:                        MapKind,
-	reflect.TypeFor[Union]().Kind():    UnionKind,
-	reflect.TypeFor[Variable]().Kind(): VariableKind,
-	reflect.Float32:                    FloatKind,
-	reflect.Float64:                    FloatKind,
-	reflect.TypeFor[BlazeObjectType]().Kind(): BlazeObjectTypeKind,
-	reflect.Interface:                         BlazeObjectIdKind,
-	reflect.TypeFor[time.Time]().Kind():       TimeValueKind,
+var NativeTypeBind = map[reflect.Type]BaseKind{
+	reflect.TypeFor[int8]():            IntegerKind,
+	reflect.TypeFor[int16]():           IntegerKind,
+	reflect.TypeFor[int32]():           IntegerKind,
+	reflect.TypeFor[int64]():           IntegerKind,
+	reflect.TypeFor[uint8]():           IntegerKind,
+	reflect.TypeFor[uint16]():          IntegerKind,
+	reflect.TypeFor[uint32]():          IntegerKind,
+	reflect.TypeFor[uint64]():          IntegerKind,
+	reflect.TypeFor[string]():          StringKind,
+	reflect.TypeFor[[]byte]():          BinaryKind, // blob
+	reflect.TypeFor[struct{}]():        StructKind,
+	reflect.TypeFor[[]any]():           ListKind,
+	reflect.TypeFor[map[any]any]():     MapKind,
+	reflect.TypeFor[Union]():           UnionKind,
+	reflect.TypeFor[Variable]():        VariableKind,
+	reflect.TypeFor[float32]():         FloatKind,
+	reflect.TypeFor[float64]():         FloatKind,
+	reflect.TypeFor[BlazeObjectType](): BlazeObjectTypeKind,
+	reflect.TypeFor[BlazeObjectId]():   BlazeObjectIdKind,
+	reflect.TypeFor[time.Time]():       TimeValueKind,
 }

1ade0f702943533cae306466cfa2fc45c7d3d5bb
Admin <2762713521@qq.com>
2024-10-17 13:13:30 +0800

    NativeTypeBind

--- native.go
+++ native.go
@@ -27,10 +27,10 @@ var NativeTypeBind = map[reflect.Type]BaseType{
 	reflect.TypeFor[uint32]():          IntegerType,
 	reflect.TypeFor[uint64]():          IntegerType,
 	reflect.TypeFor[string]():          StructType,
-	reflect.TypeFor[[]byte]():          StructType, // blob
-	reflect.TypeFor[struct{}]():        StructType, //java显示tdf就是结构体类型,ID_TERM 0结尾
-	reflect.TypeFor[[]any]():           ListType,
-	reflect.TypeFor[map[any]any]():     MapType,
+	reflect.TypeFor[[]byte]():          StructType,   // blob
+	reflect.TypeFor[struct{}]():        StructType,   //java显示tdf就是结构体类型,ID_TERM 0结尾
+	reflect.TypeFor[[]any]():           ListType,     //java显示方法名称为Vector,类型，大小，遍历
+	reflect.TypeFor[map[any]any]():     MapType,      //k v type,size,遍历
 	reflect.TypeFor[Union]():           UnionType,    //todo mock enum
 	reflect.TypeFor[Variable]():        VariableType, //VariableTdfContainer ID_TERM 0结尾
 	reflect.TypeFor[float32]():         FloatType,    //floatToIntBits
